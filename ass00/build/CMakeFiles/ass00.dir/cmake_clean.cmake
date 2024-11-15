5e57cfafb71a076ba35251642800c36c80a9e9d4
EvaS.Lu <evashenlu@gmail.com>
2024-11-15 16:47:20 -0500

    Initial commit

---
+++ ass00/build/CMakeFiles/ass00.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass00.dir/main.cpp.o"
+  "CMakeFiles/ass00.dir/main.cpp.o.d"
+  "ass00"
+  "ass00.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ass00/build/CMakeFiles/ass00.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ass00.
+# This may be replaced when dependencies are built.
