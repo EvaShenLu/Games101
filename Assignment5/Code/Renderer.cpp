2d39bff77c423ce849eb31c82641c958db8d6abf
EvaS.Lu <evashenlu@gmail.com>
2024-12-28 17:10:02 -0500

    updated ass05

--- Assignment5/Code/Renderer.cpp
+++ Assignment5/Code/Renderer.cpp
@@ -229,8 +229,9 @@ void Renderer::Render(const Scene& scene)
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
-
-            Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
+            x = scale * imageAspectRatio * (2 * (i + 0.5) / (float)scene.width - 1);
+            y = -1 * scale * (2 * (j + 0.5) / (float)scene.height - 1);
+            Vector3f dir = normalize(Vector3f(x, y, -1)); // Don't forget to normalize this direction!
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
@@ -241,9 +242,9 @@ void Renderer::Render(const Scene& scene)
     (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
     for (auto i = 0; i < scene.height * scene.width; ++i) {
         static unsigned char color[3];
-        color[0] = (char)(255 * clamp(0, 1, framebuffer[i].x));
-        color[1] = (char)(255 * clamp(0, 1, framebuffer[i].y));
-        color[2] = (char)(255 * clamp(0, 1, framebuffer[i].z));
+        color[0] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].x));
+        color[1] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].y));
+        color[2] = (unsigned char)(255 * clamp(0, 1, framebuffer[i].z));
         fwrite(color, 1, 3, fp);
     }
     fclose(fp);    
--- Assignment5/Code/Triangle.hpp
+++ Assignment5/Code/Triangle.hpp
@@ -11,7 +11,24 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
-    return false;
+    Vector3f E1 = v1 - v0;
+    Vector3f E2 = v2 - v0;
+    Vector3f S = orig - v0;
+    Vector3f S1 = crossProduct(dir, E2);
+    Vector3f S2 = crossProduct(S, E1);
+
+    // Moller-Trumbore
+    tnear = dotProduct(S2, E2) / dotProduct(S1, E1);
+    u     = dotProduct(S1, S) / dotProduct(S1, E1);
+    v     = dotProduct(S2, dir) / dotProduct(S1, E1);
+    if (tnear >0 && (u >= 0 && u <= 1) && (v >= 0 && v <= 1) && (1-u-v >= -0.00001)) //这里1-u-v若直接>=0会出现蓝点
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 class MeshTriangle : public Object
